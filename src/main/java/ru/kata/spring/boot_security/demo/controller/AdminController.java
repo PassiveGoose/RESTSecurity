ee0c57adfec9eb1c47c3d681b8b38db73c89abb2
passivegoose <boris.mikhail09200@gmail.com>
2024-08-31 22:29:21 +0300

    solved

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,15 +1,12 @@
 package ru.kata.spring.boot_security.demo.controller;
 
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,10 +14,10 @@ import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.sequrity.UserDetailsImpl;
 import ru.kata.spring.boot_security.demo.service.UserService;
-import ru.kata.spring.boot_security.demo.util.UserValidator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/admin")
@@ -28,87 +25,80 @@ public class AdminController {
 
     private final UserService userService;
 
-    private final UserValidator userValidator;
-
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public AdminController(UserService userService, UserValidator userValidator, PasswordEncoder passwordEncoder) {
+    public AdminController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
-        this.userValidator = userValidator;
         this.passwordEncoder = passwordEncoder;
     }
 
-    @GetMapping("/users")
+    @GetMapping("")
     public String printUsers(ModelMap model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
         model.addAttribute("user", userDetails.getUser());
         model.addAttribute("users", userService.getAllUsers());
         return "admin/user_list";
     }
 
-    @GetMapping("/add_user")
-    public String showAddUserPage(ModelMap model) {
-        List<Role> roles = new ArrayList<>();
-        roles.add(new Role());
-        roles.add(new Role("ADMIN"));
-
-        model.addAttribute("user", new User());
-        model.addAttribute("roles", roles);
-        return "admin/add_user";
-    }
-
-    @PostMapping(value = "/add_user", params = "action=cancel")
-    public String cancelAdding() {
-        return "redirect:/admin/users";
-    }
-
-    @PostMapping(value = "/add_user", params = "action=create")
-    public String addUser(@ModelAttribute("user") @Valid User user,
-                          BindingResult bindingResult) {
-        userValidator.validate(user, bindingResult);
-        if (!bindingResult.hasErrors()) {
-            user.setPassword(passwordEncoder.encode(user.getPassword()));
-            userService.saveUser(user);
-            return "redirect:/admin/users";
+    @PostMapping(value = "/add_user")
+    public String addUser(@RequestParam("name") String name,
+                          @RequestParam("surname") String surname,
+                          @RequestParam("age") int age,
+                          @RequestParam("username") String username,
+                          @RequestParam("password") String password,
+                          @RequestParam List<String> roles) {
+        Optional<User> userOpt = userService.getUserByUsername(username);
+        if (userOpt.isPresent()) {
+            return "redirect:/admin?error";
         }
-        return "redirect:add_user?error";
+        User user = new User();
+        user.setName(name);
+        user.setSurname(surname);
+        user.setAge(age);
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        roles.forEach(role -> {
+            if (role != null) {
+                user.addRole(new Role(role));
+            }
+        });
+        userService.saveUser(user);
+        return "redirect:/admin";
     }
 
     @PostMapping(value = "/delete_user", params = "id")
     public String deleteUser(@RequestParam int id) {
         userService.removeUserById(id);
-        return "redirect:/admin/users";
+        return "redirect:/admin";
     }
 
-    @GetMapping(value = "/edit_user", params = "id")
-    public String showEditUserPage(ModelMap model, @RequestParam int id) {
-        List<Role> roles = new ArrayList<>();
-        roles.add(new Role());
-        roles.add(new Role("ADMIN"));
-
-        User user = userService.getUserById(id);
-        model.addAttribute("user", user);
-        model.addAttribute("userId", id);
-        model.addAttribute("roles", roles);
-        return "admin/edit_user";
-    }
-
-    @PostMapping(value = "/edit_user", params = "action=cancel")
-    public String cancelEditUser() {
-        return "redirect:/admin/users";
-    }
-
-    @PostMapping(value = "/edit_user", params = "action=update")
-    public String editUser(@ModelAttribute("user") @Valid User user,
-                           BindingResult bindingResult) {
-        userValidator.validate(user, bindingResult);
-        if (!bindingResult.hasErrors()) {
-            user.setPassword(passwordEncoder.encode(user.getPassword()));
-            userService.updateUser(user);
-            return "redirect:/admin/users";
+    @PostMapping(value = "/edit_user")
+    public String editUser(@RequestParam int id,
+                           @RequestParam("name") String name,
+                           @RequestParam("surname") String surname,
+                           @RequestParam("age") int age,
+                           @RequestParam("username") String username,
+                           @RequestParam List<String> roles) {
+        Optional<User> userOpt = userService.getUserByUsername(username);
+        if (userOpt.isPresent() && userOpt.get().getId() != id) {
+            return "redirect:/admin?error";
         }
-        return "redirect:edit_user?error&id=" + user.getId();
+        User user = userService.getUserById(id);
+        user.setName(name);
+        user.setSurname(surname);
+        user.setAge(age);
+        user.setUsername(username);
+        List<Role> newRoles = new ArrayList<>();
+        roles.forEach(role -> {
+            if (role != null) {
+                newRoles.add(new Role(role));
+            }
+        });
+        user.setRoles(newRoles);
+        userService.updateUser(user);
+        return "redirect:/admin";
     }
 }
